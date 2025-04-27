109ed3ddca2a27b6161b282cbeb619c14161d356
snehalkatare2897 <kataresnehal28@gmail.com>
2025-04-27 14:20:45 +0530

    format document

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const http = require("http");
 const bodyParser = require ('body-parser');
 require("dotenv").config();
 // const fileUpload = require('express-fileupload');
-app.use(express.json());
 // app.use(fileUpload());
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -18,7 +18,7 @@ const CategoryRouter = require("./src/routes/category");
 app.use("/",userRouter);
 app.use("/", productRouter);
 app.use("/", CategoryRouter);
-// console.log(pool.connect())
+
 
 const server = http.createServer(app).listen(7777, () => {
           console.log("Server is successfully listening on port 7777...");
